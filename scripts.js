9c310ad5340667fc5773ab2de7658e4607c76332
ورود الرشيدي <66529914+wroodalrashidi@users.noreply.github.com>
2020-10-10 22:30:47 +0300

    finally done!!!!

--- scripts.js
+++ scripts.js
@@ -6,26 +6,26 @@ function gradeCalc() {
     sumOfNums += parseInt(nums[i]);
   }
   document.getElementById("sum").innerHTML = sumOfNums;
+  let image = document.createElement("hello");
 
-
-          if (sumOfNums >= 60 && sumOfNums <=69) {
-            document.getElementById("final_grade").innerHTML = "D";
+          if (sumOfNums >= 60 && sumOfNums <=69 ) {
+            document.getElementById("hello").innerHTML = "<img src='https://media1.giphy.com/media/26xBPEGoTAWdsZ8K4/giphy.gif' />";
      
              } else if (sumOfNums >= 70 && sumOfNums <= 79) {
-             document.getElementById("final_grade").innerHTML = "C";
-     
+             document.getElementById("hello").innerHTML = "<img src='https://i.gifer.com/2zH3.gif' />";
              } else if (sumOfNums >= 80 && sumOfNums <= 89) {
-             document.getElementById("final_grade").innerHTML = "B";
+              document.getElementById("hello").innerHTML = "<img src='https://i.gifer.com/2eSf.gif' />";
      
              } else if (sumOfNums >= 90) {
-             document.getElementById("final_grade").innerHTML = "A";
+              document.getElementById("hello").innerHTML = "<img src='https://i.gifer.com/2a9y.gif' />";
      
              } else {
-             document.getElementById("final_grade").innerHTML = "F";
+              document.getElementById("hello").innerHTML = "<img src='https://media2.giphy.com/media/l3q2PG0N4D2kUua3e/giphy.gif' />";
+
              }
                        
             var display=document.getElementById('final_grade');
             
             }
 
-            
+           
